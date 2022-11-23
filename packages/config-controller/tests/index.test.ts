4ea3154c8facdbdfc10aef022d30422bd82f12f3
TheGreatAxios <sawyer@dirtroad.dev>
2022-11-22 21:53:17 -0800

    Pretesting Function Setup

--- packages/config-controller/tests/index.test.ts
+++ packages/config-controller/tests/index.test.ts
@@ -1,1 +1,39 @@
-describe("Config Controller Core Testing", () => {});
+import { ConfigController } from "../src";
+
+describe("Config Controller Core Testing", () => {
+    describe("Public Functions", () => {
+        test("isFCDEnabled() => boolean", async() => {})
+        test("isMTMEnabled() => boolean", async() => {})
+        test("isAddressWhitelisted() => boolean", async() => {})
+    })
+    describe("enableMTM()", async() => {
+        test("No Signer() => boolean", async() => {})
+        test("Has Signer -> No Checks() => boolean", async() => {})
+        test("Has Signer -> Run Checks() => boolean", async() => {})
+    });
+    describe("disableMTM()", async() => {
+        test("No Signer() => boolean", async() => {})
+        test("Has Signer -> No Checks() => boolean", async() => {})
+        test("Has Signer -> Run Checks() => boolean", async() => {})
+    });
+    describe("disableFreeContractDeployment()", async() => {
+        test("No Signer() => boolean", async() => {})
+        test("Has Signer -> No Checks() => boolean", async() => {})
+        test("Has Signer -> Run Checks() => boolean", async() => {})
+    });
+    describe("enableFreeContractDeployment()", async() => {
+        test("No Signer() => boolean", async() => {})
+        test("Has Signer -> No Checks() => boolean", async() => {})
+        test("Has Signer -> Run Checks() => boolean", async() => {})
+    });
+    describe("setVersion()", async() => {
+        test("No Signer() => boolean", async() => {})
+        test("Has Signer -> No Checks() => boolean", async() => {})
+        test("Has Signer -> Run Checks() => boolean", async() => {})
+    })
+    describe("Private Functions", () => {
+        describe("mtmChecks()", () => {})
+        describe("fcdChecks()", () => {})
+    })
+});
+
