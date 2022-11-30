755fd845420a8b97f3b9e19b836599efdfd862a8
TheGreatAxios <sawyer@dirtroad.dev>
2022-11-29 18:13:48 -0800

    Test Fix Updated

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,10 @@
 export default {
-  collectCoverage: true,
-  coverageDirectory: "coverage",
   coverageReporters: [
       "json-summary"
   ],
+  maxWorkers: "75%",
+
+  // A preset that is used as a base for Jest's configuration
+  preset: "ts-jest",
   projects: ['<rootDir>/packages/*'],
 };
