78d8d9b0747870f3538f65e5a6d4a1d706db9a38
TheGreatAxios <sawyer@dirtroad.dev>
2022-12-02 13:39:30 -0800

    Fix Alpha Removal

--- scripts/unpublish/index.js
+++ scripts/unpublish/index.js
@@ -1,8 +1,15 @@
 const alpha = require("./alpha");
 
 async function main() {
+    //// Must be in this order
+    ///  Reversal of Build Order to avoid hitting dependency check
     const packages = [
-        "utils"
+        "skale.js",
+        "config-controller",
+        "context",
+        "etherbase",
+        "utils",
+        "constants"
     ]
 
     const alphaRepsonse = await Promise.all(packages.map((packageName) => alpha(packageName)))
