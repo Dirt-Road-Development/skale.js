18583005dcb86b7e9283394e7be7bca709685fde
TheGreatAxios <sawyer@dirtroad.dev>
2022-12-08 14:16:38 -0800

    Cleanup Context

--- packages/context/src/context.ts
+++ packages/context/src/context.ts
@@ -1,3 +1,12 @@
+/**
+ * @license MIT
+ * @copyright (c) 2022 Dirt Road Dev
+ * @package @skaleproject/context
+ *
+ * @file contract.ts
+ * @author Sawyer Cutler
+*/
+
 import { Addresses } from "@skaleproject/constants/lib/addresses";
 import ContextABI from "./abi.json";
 import assert from "assert";
@@ -5,8 +14,18 @@ import { INewOwner } from "./interfaces";
 import { BaseContract, IInitParams } from "@skaleproject/utils/lib/contracts/base_contract";
 import { ContractReceipt, utils } from "ethers";
 
+/**
+ * @class Context
+ */
 export class Context extends BaseContract {
-
+    
+    /**
+     * 
+     * Initialization of the ConfigController Contract
+     * This is a wrapper of the solidity smart contract 
+     * 
+     * @param {IInitParams} params - The core parameters passed into the constructor
+     */
     constructor(params: IInitParams) {
         super({
             ...params,
@@ -17,8 +36,12 @@ export class Context extends BaseContract {
 
     /**
      * 
-     * @returns string -> SKALE Chain Name
-     * @description This value is set when predeployed
+     * Gets name of schain
+     * 
+     * @function getSchainName
+     * 
+     * @returns {Promise<string>} - SKALE Chain Name
+     * 
      */
     public async getSchainName() : Promise<string> {
         return await this.contract.getSchainName();
@@ -26,8 +49,11 @@ export class Context extends BaseContract {
 
     /**
      * 
-     * @returns string -> owner address
-     * @description This value can be changed, but is set to Marionette by Deafult
+     * Gets Schain Owner Address
+     * 
+     * @function getSchainOwnerAddress
+     * 
+     * @returns {Promise<string>} - owner address of schain
      */
     public async getSchainOwnerAddress() : Promise<string> {
         return await this.contract.getSchainOwnerAddress();
@@ -35,13 +61,17 @@ export class Context extends BaseContract {
 
     /**
      * 
-     * @param params -> The Owner address replacement value
+     * Allows owner to update the owner address
+     * 
+     * @function setSchainOwnerAddress
+     * 
+     * @param {string} address - The new owner address
      * @returns ContractReceipt of the transaction
      */
-    public async setSchainOwnerAddress(params: INewOwner) : Promise<ContractReceipt> {
+    public async setSchainOwnerAddress({ address } : INewOwner) : Promise<ContractReceipt> {
         this.checkSigner();
-        assert(utils.isAddress(params.address), "Invalid Ethereum  Address");
-        const res = await this.contract.setSchainOwnerAddress(params.address);
+        assert(utils.isAddress(address), "Invalid Ethereum  Address");
+        const res = await this.contract.setSchainOwnerAddress(address);
         return res;
     }
 }
