db67ae85a1fd73fe46937d45ddaf663a83bfdc27
TheGreatAxios <sawyer@dirtroad.dev>
2023-02-11 13:17:27 -0800

    Minor Tweaks and Wallet Creation

--- packages/pow-ethers/src/anonymous.ts
+++ packages/pow-ethers/src/anonymous.ts
@@ -1,50 +1,19 @@
 import { Wallet } from "@ethersproject/wallet";
 import { JsonRpcProvider, } from "@ethersproject/providers";
 import SkalePowMiner from "@skaleproject/pow/src/index";
-import BN from "bn.js/index";
-import { Interface } from "ethers/lib/utils";
-import { AnonymousParams, TransactionParams, EncodeSendParams } from "./types";
-import { BigNumber } from "@ethersproject/bignumber";
-import BaseMiner from "./miner";
+import { AnonymousParams } from "./types";
+import WalletPow from "./wallet";
 
-export default class AnonymousPoW extends BaseMiner {
+export default class AnonymousPoW extends WalletPow {
   
-  protected provider: JsonRpcProvider;
   protected miner: SkalePowMiner;
-  protected wallet: Wallet;
+  protected provider: JsonRpcProvider;
 
   constructor(params: AnonymousParams) {
-    super(params.difficulty);
-    this.provider = new JsonRpcProvider(params.rpcUrl);
-    this.wallet = Wallet.createRandom().connect(this.provider);
-  }
-  
-  public async send(params: TransactionParams) {
-
-    const { to, data } = params;
-
-    let nonce = await this.wallet.getTransactionCount();
-    let gas = new BN(100000);
-    const mineFreeGasResult = await this.miner.mineGasForTransaction(nonce, gas, this.wallet.address);
-
-    return await this.wallet.sendTransaction({
-      from: this.wallet.address,
-      to,
-      data,
-      nonce,
-      gasPrice: BigNumber.from(mineFreeGasResult),
-    }) 
-  }
-
-  public async encodeAndSend(params: EncodeSendParams) {
-  
-    const { abi, functionArgs, functionName} = params;
-    
-    const contractInterface = new Interface(abi);
-    const data = contractInterface.encodeFunctionData(functionName, functionArgs);
-
-    return await this.send({ ...params, data });
-
+    const _provider = new JsonRpcProvider(params.rpcUrl);
+    const _wallet = Wallet.createRandom().connect(_provider);
+    super({ difficulty: params.difficulty, wallet: _wallet });
+    this.provider = _provider;
   }
 }
 
--- packages/pow-ethers/src/wallet.ts
+++ packages/pow-ethers/src/wallet.ts
@@ -1,8 +1,11 @@
 import { Wallet } from "@ethersproject/wallet";
 import SkalePowMiner from "@skaleproject/pow/src";
 import assert from "assert";
+import { BN } from "bn.js";
+import { BigNumber } from "ethers";
+import { Interface } from "ethers/lib/utils";
 import BaseMiner from "./miner";
-import { WalletParams } from "./types";
+import { EncodeSendParams, TransactionParams, WalletParams } from "./types";
 
 export default class WalletPow extends BaseMiner {
 
@@ -14,4 +17,32 @@ export default class WalletPow extends BaseMiner {
         assert(params.wallet.provider._isProvider, "Wallet does not have a provider");
         this.wallet = params.wallet;
     }
-}
+    
+    public async send(params: TransactionParams) {
+
+        const { to, data } = params;
+    
+        let nonce = await this.wallet.getTransactionCount();
+        let gas = new BN(params.gas ?? 100000);
+        const mineFreeGasResult = await this.miner.mineGasForTransaction(nonce, gas, this.wallet.address);
+    
+        return await this.wallet.sendTransaction({
+          from: this.wallet.address,
+          to,
+          data,
+          nonce,
+          gasPrice: BigNumber.from(mineFreeGasResult),
+        }) 
+      }
+    
+      public async encodeAndSend(params: EncodeSendParams) {
+      
+        const { abi, functionArgs, functionName} = params;
+        
+        const contractInterface = new Interface(abi);
+        const data = contractInterface.encodeFunctionData(functionName, functionArgs);
+    
+        return await this.send({ ...params, data });
+      }
+    }
+    
