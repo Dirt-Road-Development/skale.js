944aefc063c94df7f2d64b6f9d6e329512c4d746
TheGreatAxios <sawyer@dirtroad.dev>
2023-02-14 16:37:12 -0800

    Cleanup Complete

--- packages/pow-ethers/src/wallet.ts
+++ packages/pow-ethers/src/wallet.ts
@@ -9,40 +9,40 @@ import { EncodeSendParams, TransactionParams, WalletParams } from "./types";
 
 export default class WalletPow extends BaseMiner {
 
-    protected wallet: Wallet;
-    protected miner: SkalePowMiner;
+	protected wallet: Wallet;
+	protected miner: SkalePowMiner;
 
-    constructor(params: WalletParams) {
-        super(params.difficulty);
-        assert(params.wallet.provider._isProvider, "Wallet does not have a provider");
-        this.wallet = params.wallet;
-    }
-    
-    public async send(params: TransactionParams) {
+	constructor(params: WalletParams) {
+			super(params.difficulty);
+			assert(params.wallet.provider._isProvider, "Wallet does not have a provider");
+			this.wallet = params.wallet;
+	}
 
-        const { to, data } = params;
-    
-        let nonce = await this.wallet.getTransactionCount();
-        let gas = new BN(params.gas ?? 100000);
-        const mineFreeGasResult = await this.miner.mineGasForTransaction(nonce, gas, this.wallet.address);
-    
-        return await this.wallet.sendTransaction({
-          from: this.wallet.address,
-          to,
-          data,
-          nonce,
-          gasPrice: BigNumber.from(mineFreeGasResult),
-        }) 
-      }
-    
-      public async encodeAndSend(params: EncodeSendParams) {
-      
-        const { abi, functionArgs, functionName} = params;
-        
-        const contractInterface = new Interface(abi);
-        const data = contractInterface.encodeFunctionData(functionName, functionArgs);
-    
-        return await this.send({ ...params, data });
-      }
-    }
-    
+	public async send(params: TransactionParams) {
+
+		const { to, data } = params;
+
+		let nonce = await this.wallet.getTransactionCount();
+		let gas = new BN(params.gas ?? 100000);
+		const mineFreeGasResult = await this.miner.mineGasForTransaction(nonce, gas, this.wallet.address);
+
+		return await this.wallet.sendTransaction({
+			from: this.wallet.address,
+			to,
+			data,
+			nonce,
+			gasPrice: BigNumber.from(mineFreeGasResult)
+		}) 
+	}
+
+	public async encodeAndSend(params: EncodeSendParams) {
+	
+		const { abi, functionArgs, functionName} = params;
+		
+		const contractInterface = new Interface(abi);
+		const data = contractInterface.encodeFunctionData(functionName, functionArgs);
+
+		return await this.send({ ...params, data });
+	}
+}
+		
